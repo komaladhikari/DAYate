a1f4644dc17470638ab074e18845bcfc3ad036ff
komaladh33 <160463322+jrb454@users.noreply.github.com>
2026-05-28 07:22:05 -0500

    imported and exported multer, api checked, it works

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 
 import express from 'express';
 import {addDates, listDates, removeDates} from '../controllers/productController.js';
+import upload from '../middleware/multer.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add',upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]), addDates.products);
+productRouter.post('/add',upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1}]), addDates.products);
 productRouter.get('/list', listDates.products);
 productRouter.post('/remove', removeDates.products);
 
